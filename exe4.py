278a267d1bd1b12349777eef25c3f4a7f533b22c
Vinicius <89617179+Peretta@users.noreply.github.com>
2024-03-16 14:17:02 -0300

    Update exe4.py

--- exe4.py
+++ exe4.py
@@ -7,7 +7,7 @@ Desligue o primeiro interruptor
 Vá em cada sala, e toque na lâmpada
 
 Se a lampada estiver quente, corresponde ao interruptor um
-Se a lampada estiver morna, corresponde ao interruptor dois
+Se a lampada estiver morna e acesa, corresponde ao interruptor dois
 se a lampada estiver fria, corresponta ao interrupor três
 
 
